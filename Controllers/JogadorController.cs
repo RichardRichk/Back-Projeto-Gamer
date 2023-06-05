d2964672d46eb21f5b81d8638bc9d40dea8c764d
Richard Fernandes Passarelli <45811737874@info.local>
2023-06-05 08:50:02 -0300

    Falta realizar tabela no index-Jogadores

--- Controllers/JogadorController.cs
+++ Controllers/JogadorController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Back_Projeto_Gamer.Infra;
+using Back_Projeto_Gamer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -21,12 +22,29 @@ namespace Back_Projeto_Gamer.Controllers
 
         Context c = new Context();
 
+        [Route("Listar")]
         public IActionResult Index()
         {
             ViewBag.Jogador = c.Jogador.ToList();
             ViewBag.Equipe = c.Equipe.ToList();
             
             return View();
+        }
+
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Jogador novoJogador = new Jogador();
+
+            novoJogador.Name = form["Name"].ToString();
+
+            novoJogador.Email = form["Email"].ToString();
+
+            novoJogador.Senha = form["Senha"].ToString();
+
+            novoJogador.IdEquipe = int.Parse(form["NameEquipe"]);
+
+            return LocalRedirect("~/Jogador/Listar");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
